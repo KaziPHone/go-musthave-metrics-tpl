141f2ce2a02b788582be4eb6ea8cd9dacc6eb655
kaziphone <kazimirskiy87@gmail.com>
2025-10-18 23:18:55 +0300

    fix handler get metric

--- cmd/server/handlers/getMetricHandler.go
+++ cmd/server/handlers/getMetricHandler.go
@@ -13,16 +13,16 @@ func (h *Handler) GetMetricHandler(w http.ResponseWriter, r *http.Request) {
 	typeMetric := chi.URLParam(r, "typeMetric")
 	nameMetric := chi.URLParam(r, "nameMetric")
 
-	metric, ok := h.Storage.MetricTypes[typeMetric]
+	metric, ok := h.Storage.MetricTypes[nameMetric]
 
-	if !ok || !(nameMetric == "gauge" || nameMetric == "counter") {
+	if !ok || !(typeMetric == "gauge" || typeMetric == "counter") {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	if nameMetric == "gauge" {
-		fmt.Fprintf(w, "%s: Gauge=%v", typeMetric, strconv.FormatFloat(metric.Gauge, 'f', -1, 64))
+	if typeMetric == "gauge" {
+		fmt.Fprintln(w, strconv.FormatFloat(metric.Gauge, 'f', -1, 64))
 	} else {
-		fmt.Fprintf(w, "%s: Counter=%v", typeMetric, strconv.FormatInt(metric.Counter, 10))
+		fmt.Fprintln(w, strconv.FormatInt(metric.Counter, 10))
 	}
 }
