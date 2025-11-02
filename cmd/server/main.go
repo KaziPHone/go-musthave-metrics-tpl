4f118b5dbac90678693096732122e5655f88d3d7
kaziphone <kazimirskiy87@gmail.com>
2025-11-02 18:25:27 +0300

    add logger in server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/KaziPHone/go-musthave-metrics-tpl/internal/config"
 	"github.com/KaziPHone/go-musthave-metrics-tpl/pkg/handlers"
 	"github.com/KaziPHone/go-musthave-metrics-tpl/pkg/storage"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -16,7 +15,7 @@ func main() {
 	cfg := config.NewConfigServer()
 
 	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	router.Use(handlers.LoggingMiddleware)
 	memStorage := storage.NewMemStorage()
 	h := &handlers.Handler{Storage: memStorage}
 
@@ -24,9 +23,9 @@ func main() {
 	router.Get("/value/{typeMetric}/{nameMetric}", h.GetMetricHandler)
 	router.Post("/update/{typeMetric}/{nameMetric}/{value}", h.UpdateHandler)
 
-	log.Printf("Starting server on:%s...", cfg.Host)
+	log.Printf("Starting server on: %s...", cfg.Host)
 	err := http.ListenAndServe(cfg.Host, router)
 	if err != nil {
-		log.Fatal(err)
+		log.Err(err)
 	}
 }
---
+++ pkg/handlers/loggingMiddleware.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+type responseRecorder struct {
+	http.ResponseWriter     // Исходный ResponseWriter
+	statusCode          int // Код состояния
+	responseSize        int // Размер ответа
+}
+
+func (rr *responseRecorder) WriteHeader(statusCode int) {
+	rr.statusCode = statusCode
+	rr.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rr *responseRecorder) Write(b []byte) (int, error) {
+	n, err := rr.ResponseWriter.Write(b)
+	rr.responseSize += n
+	return n, err
+}
+
+func LoggingMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusBadRequest, // По умолчанию считаем OK
+		}
+
+		h.ServeHTTP(recorder, r)
+
+		log.Info().
+			Str("method", r.Method).
+			Str("uri", r.RequestURI).
+			Int("status_code", recorder.statusCode).
+			Int("response_size_bytes", recorder.responseSize).
+			Dur("request_duration_ms", time.Since(start)).
+			Msg("")
+	})
+}
