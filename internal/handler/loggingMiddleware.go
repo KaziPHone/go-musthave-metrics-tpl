e5cde42d337df6b8867379b1ac0b54a089979753
kaziphone <kazimirskiy87@gmail.com>
2025-11-03 18:30:05 +0300

    iter7

--- internal/handler/loggingMiddleware.go
+++ internal/handler/loggingMiddleware.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -11,6 +13,7 @@ type responseRecorder struct {
 	http.ResponseWriter     // Исходный ResponseWriter
 	statusCode          int // Код состояния
 	responseSize        int // Размер ответа
+	bodyBuf             bytes.Buffer
 }
 
 func (rr *responseRecorder) WriteHeader(statusCode int) {
@@ -21,12 +24,20 @@ func (rr *responseRecorder) WriteHeader(statusCode int) {
 func (rr *responseRecorder) Write(b []byte) (int, error) {
 	n, err := rr.ResponseWriter.Write(b)
 	rr.responseSize += n
+	rr.bodyBuf.Write(b)
 	return n, err
 }
 
 func LoggingMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
+
+		// Читаем тело запроса
+		bodyBytes, _ := ioutil.ReadAll(r.Body)
+		r.Body.Close() // закрываем оригинальный поток
+
+		// Восстанавливаем тело запроса обратно в поток
+		r.Body = ioutil.NopCloser(bytes.NewReader(bodyBytes))
 
 		recorder := &responseRecorder{
 			ResponseWriter: w,
@@ -41,6 +52,8 @@ func LoggingMiddleware(h http.Handler) http.Handler {
 			Int("status_code", recorder.statusCode).
 			Int("response_size_bytes", recorder.responseSize).
 			Dur("request_duration_ms", time.Since(start)).
+			Str("body", string(bodyBytes)).
+			Str("resp_body", recorder.bodyBuf.String()).
 			Msg("")
 	})
 }
