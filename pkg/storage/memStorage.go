9a56580f21453bcbdac60a58a6b57780864e0ee8
kaziphone <kazimirskiy87@gmail.com>
2025-11-03 21:43:35 +0300

    iter7

--- pkg/storage/memStorage.go
+++ pkg/storage/memStorage.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"fmt"
+	"sync"
 
 	models "github.com/KaziPHone/go-musthave-metrics-tpl/internal/model"
 )
@@ -15,6 +16,7 @@ type IStorage interface {
 
 type MStorage struct {
 	MetricTypes map[string]*MetricType
+	mu          sync.Mutex
 }
 
 type MetricType struct {
@@ -79,7 +81,8 @@ func (m *MStorage) UpdateMetric(metricName, typeMetric string, value interface{}
 }
 
 func (m *MStorage) UpdateMetricV2(metric models.Metrics) error {
-
+	m.mu.Lock()         // Заблокировали доступ другим потокам
+	defer m.mu.Unlock() // Освобождение блокировки после завершения функции
 	if _, ok := m.MetricTypes[metric.ID]; !ok {
 		m.MetricTypes[metric.ID] = &MetricType{}
 	}
