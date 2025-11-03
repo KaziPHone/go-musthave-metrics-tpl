ca8d8948bf7945f3579ab7c021fe6d106b4f9d44
kaziphone <kazimirskiy87@gmail.com>
2025-11-03 10:51:49 +0300

    iter7

--- internal/handler/loggingMiddleware.go
+++ internal/handler/loggingMiddleware.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -28,9 +30,16 @@ func LoggingMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
+		// Читаем тело запроса
+		bodyBytes, _ := ioutil.ReadAll(r.Body)
+		r.Body.Close() // закрываем оригинальный поток
+
+		// Восстанавливаем тело запроса обратно в поток
+		r.Body = ioutil.NopCloser(bytes.NewReader(bodyBytes))
+
 		recorder := &responseRecorder{
 			ResponseWriter: w,
-			statusCode:     http.StatusBadRequest, // По умолчанию считаем OK
+			statusCode:     http.StatusBadRequest,
 		}
 
 		h.ServeHTTP(recorder, r)
@@ -41,6 +50,7 @@ func LoggingMiddleware(h http.Handler) http.Handler {
 			Int("status_code", recorder.statusCode).
 			Int("response_size_bytes", recorder.responseSize).
 			Dur("request_duration_ms", time.Since(start)).
+			Str("request_body", string(bodyBytes)).
 			Msg("")
 	})
 }
